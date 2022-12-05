8669e4990023999ed5cb5ba853fdbcbc87789ac4
Furkan Ertekin <furkanertekin0@gmail.com>
2022-12-05 20:38:56 +0300

    first commit

---
+++ FreeUygulam2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeUygulam2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pay = 1;
+            decimal payda = 2;
+
+            for (int i = 0; i<= 5; i++)
+            {
+                Console.WriteLine(pay + "/" + payda);
+                pay = payda;
+                payda = payda * payda;
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
